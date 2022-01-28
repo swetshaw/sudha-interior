fb63eda06747d324ef272c174a469f83074d70e3
dip bhattacharjee <dip.bhattacharjee@postman.com>
2022-01-28 22:54:47 +0530

    removing antd package support, added default navbar style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import logo from './logo.svg';
-import { Layout } from 'antd';
 import './App.css';
-import './antd.css';
 import Navbar from './components/Navbar/Navbar';
-import HeroContainer from './components/HeroContainer/HeroContainer';
 
 
 function App() {
-  const { Header, Footer, Sider, Content } = Layout;
   return (
     <div className='App'>
       <Navbar />
-      <HeroContainer/>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,76 @@
 import React from 'react';
-import { Menu, Button } from 'antd';
 import './Navbar.css';
+import styled from 'styled-components';
+
+const StyledNavbar = styled.div`
+  display: flex;
+  flex-grow: 1;
+  flex-shrink: 1;
+  align-items: center;
+  justify-content: space-between;
+  overflow: hidden;
+  background-color: transparent;
+  padding-top: 20px;
+  // sticky navbar style
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+`;
+
+const StyledNavbarLeft = styled.div`
+  margin-left: 48px;
+  display: flex;
+  align-items: center;
+`;
+const StyledNavbarRight = styled.div`
+  margin-right: 48px;
+`;
+const StyledNavarLinks = styled.a`
+  color: var(--tyrian);
+  margin-right: 48px;
+  height: 64px;
+  cursor: pointer;
+  user-select: none;
+  &: active {
+    // add on click style here
+  }
+`;
+
+const StyledNavarConsultNowButton = styled.button`
+  width: 120px;
+  height: 32px;
+  border-radius: 4px;
+  color: var(--tyrian);
+  border: 1px solid var(--tyrian);
+  background-color: white;
+  cursor: pointer;
+  user-select: none;
+  font-family: 'Inter';
+`;
+
+const StyledLogo = styled.img``;
+const StyledBrandName = styled.div`
+  font: 'Poppins';
+  font-weight: 600;
+  font-size: 24px;
+  color: var(--tyrian);
+  margin-left: 8px;
+`;
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div>
-        <h1 className='brand-h1'>Sudha Interior</h1>
-      </div>
-      <div className='nav_links'>
-        <Menu mode='horizontal' className='nav_menu'>
-          <Menu.Item className='menu_item_home'>Home</Menu.Item>
-          <Menu.Item>Shop</Menu.Item>
-          <Menu.Item>Works</Menu.Item>
-        </Menu>
-        <Button className='btn-border'>Consult</Button>
-      </div>
-    </div>
+    <StyledNavbar className='sticky'>
+      <StyledNavbarLeft>
+        <StyledLogo src="./compass.png" />
+        <StyledBrandName>Sudha Interior</StyledBrandName>
+      </StyledNavbarLeft>
+      <StyledNavbarRight>
+        <StyledNavarLinks>Home</StyledNavarLinks>
+        <StyledNavarLinks>Shop</StyledNavarLinks>
+        <StyledNavarConsultNowButton>Consult Now</StyledNavarConsultNowButton>
+      </StyledNavbarRight>
+    </StyledNavbar>
   );
 };
 
